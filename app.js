2c0664638e220bb31dc15e6996afb2e91e2ca5c9
Muhammad <muhammad.ali.72.27@gmail.com>
2020-11-23 21:29:28 -0800

    added app to serve the distributable javascript/css/images

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app = express();
 const path = require('path');
 
 
-
+app.use('/', express.static(path.join(__dirname, '/dist/')));
 
 app.get('/', (req, res) => {
-    res.send(path.join(__dirname, '/dist/') + 'index.html');
+    console.log(path.join(__dirname, '/dist/') + 'index.html');
+    res.sendFile(path.join(__dirname, '/dist/') + 'index.html');
 } )
 
 module.exports = app;
