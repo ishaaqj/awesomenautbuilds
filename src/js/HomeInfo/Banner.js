5758ae666f910dab03f95d2526375bda7d5b05ec
Muhammad <muhammad.ali.72.27@gmail.com>
2020-11-26 20:10:07 -0800

    added generated components from description array and added quick styling to distinguish in building

--- src/js/HomeInfo/Banner.js
+++ src/js/HomeInfo/Banner.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 
 const Banner = (props) =>{
     return (
-        <div>
+        <div class="Banner" style={{textAlign: "center"}}>
             Awesomenauts {"\n"}<span>Guides</span>
         </div>
     )
--- src/js/HomeInfo/Homepage.js
+++ src/js/HomeInfo/Homepage.js
@@ -5,12 +5,31 @@ import Promo from './Promo';
 
 
 class HomePage extends React.Component{
-    render(){
 
+    render(){
+        const promoInfo = [
+            {
+                title: "Register Now!",
+                picture: "null",
+                description: "Registered users can leave comments and submit own guides. Moreover, you will see less advertisement on the website"
+            },
+            {
+                title: "Submit you guides",
+                picture: "null",
+                description: "Do you think you know how to play Awesomenauts better? Share your own guide with the community and earn rewards"
+            },
+            {
+                title: "Top guides",
+                picture: "null",
+                description: "Try out Awesomenauts guides with the highest ratings selected by our community"
+            }
+        ];
         return(
             <div className="Homepage">
                 <Banner/>
-                <Promo picture="null" title="example Title" description="example Description"/>
+                {promoInfo.map(el => {
+                    return <Promo picture={el.picture} title={el.title} description={el.description}/>
+                })}
             </div>
         )
     }
