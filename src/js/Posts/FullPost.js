8a6c937f4b2002a5fd7b3e9abc73645142985c0a
Muhammad <muhammad.ali.72.27@gmail.com>
2020-12-14 21:05:16 -0800

    replaced data to be dynamic and updated db to include title

--- src/js/Posts/FullPost.js
+++ src/js/Posts/FullPost.js
@@ -1,42 +1,45 @@
 import React from 'react';
 import Comment from '../User/Comment';
 import axios from 'axios';
-import {withRouter} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 class FullPost extends React.Component {
-
-
-    state = {
-        id: -1,
-        watchCount: 0,
-        upvote: 0,
+	state = {
+		id: -1,
+		watchCount: 0,
+		upvote: 0,
 		cost: 0,
 		username: '',
 		buildName: '',
 		date: 'yyyy-mm-dd',
-		instructions: ''
-    }
+		instructions: '',
+		title: ''
+	};
 
-    componentDidMount(){
-
+	componentDidMount() {
 		// This will only be executed once so it is more appropriate to use Async calls in this instead of componentDidUpdate()
 		// No need for verification because it's rendered once
-		axios.get(`/dbPost/${this.props.match.params.id}/${this.props.match.params.username}`).then(res => {
-			let query = res.data.posts;
-			this.setState({
-				id: query.id,
-				watchCount: query.watchcount,
-				upvote: query.upvote,
-				cost: query.cost,
-				username: query.username,
-				buildName: query.buildname,
-				// Uses regular expression to split string and then since it returns an array, join the array to make a single string again
-				date: query.date.match(/\d{4}-\d{2}-\d{2}/g).join(""),
-				instructions: query.instructions
+		axios
+			.get(`/dbPost/${this.props.match.params.id}/${this.props.match.params.username}`)
+			.then((res) => {
+				let query = res.data.posts;
+				this.setState({
+					id: query.id,
+					watchCount: query.watchcount,
+					upvote: query.upvote,
+					cost: query.cost,
+					username: query.username,
+					buildName: query.buildname,
+					// Uses regular expression to split string and then since it returns an array, join the array to make a single string again
+					date: query.date.match(/\d{4}-\d{2}-\d{2}/g).join(''),
+					instructions: query.instructions,
+					title: query.title
+				});
 			})
-		}).catch(err => {console.log(err)});
-
-    }
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 	render() {
 		return (
 			<div>
@@ -44,21 +47,21 @@ class FullPost extends React.Component {
 					<div className="Header">
 						<div className="Header-Title">
 							<p>Picture(null)</p>
-							<h2>Ex: The Master Tank of Black Holes</h2>
+							<h2>{this.state.title}</h2>
 						</div>
 						<div className="Header-UserInfo">
-							<p>By: SomeUsername On: Null</p>
-							<div className="Header-Stats">Some User statistic</div>
+							<p>By: {this.state.username} On: {this.state.date}</p>
+							<div className="Header-Stats"> {this.state.watchCount}  {this.state.upvote}  {this.state.cost} </div>
 						</div>
 					</div>
 
-					<h2>Ex: Sentry X-58 build</h2>
+					<h2>Ex: {this.state.buildName} build</h2>
 					<div>List of pictures</div>
 					<p>Good against - list of pictures</p>
 					<p>Good with - list of pictures</p>
 					<p>Bad against - list of pictures</p>
 					<p>Bad With - list of pictures</p>
-					<h2>Sentry X-58 Guide</h2>
+					<h2>{this.state.buildName} Guide</h2>
 					<div>
 						<p>Some description</p>
 						<ol>
